1b85fa2d9983e457e87faa99eb53fd03422fbdd6
AhmadMorshed <ahmadnakawa676@gmail.com>
2024-09-23 05:42:25 -0700

    first commit

---
+++ Data.Models/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Entities
+{
+    public class Department:BaseEntity
+    {
+        [DisplayName("DepartmentName")]
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+
+    }
+}
